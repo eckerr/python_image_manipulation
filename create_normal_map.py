21c02674cbdf8277c77b1fcd4632aaa4ac04beee
Ed Kerr <edkerr@comcast.net>
2019-02-13 11:51:40 -0600

    consolidated functions

--- create_normal_map.py
+++ create_normal_map.py
@@ -6,12 +6,16 @@ Steps to creating a normal map from a grayscale image
  """
 import cv2
 import numpy as np
-from slope_math import central_dif_cols, central_dif_rows
 
 
 def read_image_to_gray(file):
     gray = cv2.imread(file, 0)
     return gray
+
+
+def invert_image(image_array):
+    """ Inverts 8 bit image """
+    return 255 - image_array
 
 
 def convert_array_to_float(array):
@@ -36,44 +40,48 @@ def convert_unsigned_channel_to_image(channel):
 
 def central_dif_cols(array):
     """ add column to each side to facilitate subtraction """
-    first_col = array[:, 0: 1]
+    first_col = array[:, 0:1]
     last_col = array[:, -1:]
     first_part = np.append(last_col, array, axis=1)
     master_array = np.append(first_part, first_col, axis=1)
     after = master_array[:, 2:]
     before = master_array[:, :-2]
     out_array = (after - before) / 2
-    return out_array * -1
+    return out_array
+
 
 def central_dif_rows(array):
     """ add row to top and bottom to facilitate subtraction """
-    first_row = array[0: 1, :]
+    first_row = array[0:1, :]
     last_row = array[-1:, :]
     first_part = np.append(last_row, array, axis=0)
     master_array = np.append(first_part, first_row, axis=0)
     after = master_array[2:, :]
     before = master_array[:-2, :]
     out_array = (after - before) / 2
-    return out_array * -1
+    return out_array
 
 
 def display_grayscale(grayscale_array):
-    cv2.imshow('grayscale', grayscale_array)
+    cv2.imshow("grayscale", grayscale_array)
 
 
-def display_channel_list(channels):
-    cv2.imshow('Blue', channels[0])
-    cv2.imshow('Green', channels[1])
-    cv2.imshow('Red', channels[2])
+def display_channel_list(channel_list):
+    cv2.imshow("Blue", channel_list[0])
+    cv2.imshow("Green", channel_list[1])
+    cv2.imshow("Red", channel_list[2])
 
 
-def build_normal_image(col_array, row_array):
-    """ start building channels from slope info """
-    # create an array to hold the channels
-    size = col_array.shape
-    blank_array = np.zeros((size[0], size[1], 3))
+def create_array_of_3_channels(size):
+    # build a blank array of needed size
+    blank_array = np.ones((size[0], size[1], 3))
     channels = []
-    channels = cv2.split(blank_array, channels)
+    return cv2.split(blank_array, channels)
+
+
+def build_normal_image(col_array, row_array, channels):
+    """ start building channels from slope info """
+    size = channels[0].shape
     for row in range(size[0]):
         for col in range(size[1]):
             dx = col_array[row, col]
@@ -85,17 +93,49 @@ def build_normal_image(col_array, row_array):
             # blue channel
             channels[0][row, col] = 1 / distance
             # green channel
-            channels[1][row, col] = dx / distance
+            channels[1][row, col] = -dx / distance
             # red channel
-            channels[2][row, col] = dy / distance
-    return convert_channels_to_images(channels)
+            channels[2][row, col] = -dy / distance
+    return channels
 
 
-def display_normal_image(channels):
-    normal_img = cv2.merge(channels)
-    cv2.imshow('normal', normal_img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+
+def rebalance_norms(channel_list):
+    size = channel_list[0].shape
+    f = np.zeros(3, dtype=np.float32)
+    for row in range(size[0]):
+        for col in range(size[1]):
+            dx = channel_list[1][row, col]
+            dy = channel_list[2][row, col]
+            dz = channel_list[0][row, col]
+            vector = np.array([dx, dy, dz], dtype=np.float32)
+            f = cv2.normalize(vector, f)
+            channel_list[1][row, col] = f[0] * 255
+            channel_list[2][row, col] = f[1] * 255
+            channel_list[0][row, col] = f[2] * 255
+    return channel_list
+
+
+def rescale_x_y(channel_float_list, scale_factor):
+    # determine max and min to scale by
+    green_range = channel_float_list[1].max() - channel_float_list[1].min()
+    red_range = channel_float_list[2].max() - channel_float_list[2].min()
+    range = max(green_range, red_range) / 2
+    max_scale = 1 / range
+    scale_val = max_scale * scale_factor
+    channel_float_list[1] *= scale_val
+    channel_float_list[2] *= scale_val
+    print(max_scale, scale_factor)
+
+    return channel_float_list
+
+
+def display_normal_image(channel_list):
+    normal_img = cv2.merge(channel_list)
+    cv2.imshow("image", normal_img)
+    return normal_img
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
 
 def convert_channels_to_images(channels):
@@ -109,32 +149,58 @@ def convert_channels_to_images(channels):
     return channels
 
 
+def create_black_window(size):
+    img = np.zeros((size[0], size[1], 3), np.uint8)
+    return cv2.namedWindow("image")
+
+
+def scale_callback(x):
+    pass
+
+
 def process_image(img_in):
     float_array = convert_array_to_float(img_in)
     n_float = normalize_float_array(float_array)
-    y_array = central_dif_rows(n_float)
-    x_array = central_dif_cols(n_float)
-    channels = build_normal_image(y_array, x_array)
+    y_norms = central_dif_rows(n_float)
+    x_norms = central_dif_cols(n_float)
+    blank_channels = create_array_of_3_channels(y_norms.shape)
+
+    raw_channel_list = build_normal_image(y_norms, x_norms, blank_channels)
+    scaled_channel_list = rescale_x_y(raw_channel_list, 1)
+    channel_list = convert_channels_to_images(scaled_channel_list)
     # display
     display_grayscale(img_in)
-    display_channel_list(channels)
-    display_normal_image(channels)
+    display_channel_list(channel_list)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+    return channel_list
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    filename = 'brick.jpg'
+    filename = "Marblefloor_diffuse.jpg"
+    filenameN = "Marblefloor_normal.jpg"
+
+    # filename = 'brick.jpg'
+    # filenameN = 'brick_normal.jpg'
+
+    # filename = 'rust_014.jpg'
+    # filenameN = 'rust_normal.jpg'
+
+    # filename = 'Voronoi.png'
+    # filenameN = 'Voronoi.png'
+
     image_in = read_image_to_gray(filename)
     original = cv2.imread(filename)
-    cv2.imshow('original', original)
-    process_image(image_in)
-
-
-
-
-
-
-
+    cv2.imshow("original", original)
+    img_window = create_black_window(image_in.shape)
+    cv2.createTrackbar("scale", "image", 0, 10, scale_callback)
+    x = cv2.getTrackbarPos("scale", "image")
+    channels_out = process_image(image_in)
+    n_img_out = display_normal_image(channels_out)
+    image_ref = cv2.imread(filenameN)
+    cv2.imshow("reference", image_ref)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    cv2.imwrite("E2_N_" + filenameN, n_img_out)
